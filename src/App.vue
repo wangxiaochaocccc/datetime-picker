2121b308beb715de6fd2460b4210a6ffd90e88c6
wangchao <wangxiaochaocccc@163.com>
2023-10-12 23:49:15 +0800

    feat:project init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class='container'>
+      <datetime-picker width="300px" v-model="val"/>
+      <div>111</div>
+      <date-range v-model="val2"/>
+      <button @click="getTime" >获取时间</button>
+    </div>
+</template>
+
+<script>
+import datetimePicker from './libs/datetime-picker.vue';
+import dateRange from './libs/date-range.vue'
+export default {
+  data () {
+    return {
+      val: '',
+      val2:[]
+    }
+  },
+  components: {
+    datetimePicker,
+    dateRange
+  },
+  methods: {
+    getTime () {
+      console.log(this.val2);
+    }
+  }
+}
+</script>
+<style lang='scss'>
+.container {
+  width: 800px;
+  height: 400px;
+  background-color:pink;
+  margin: 100px auto 0;
+}
+</style>
